e52036c91bd35efd5432e2186b7c88577f8283fc
Massimo Biagioli <biagiolimassimo@gmail.com>
2017-04-22 08:58:39 +0200

    Sistemato routing

---
+++ client-app/src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hello">
+    {{this.msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: 'Home Page'
+    }
+  }
+}
+</script>
